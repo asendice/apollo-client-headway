0dbbc77cae17c80a9abdb6a8ebfd4daea225465a
Dylan Travis <49328834+asendice@users.noreply.github.com>
2022-03-07 15:57:40 -0800

    nav responsive hid links until larger screen sizes, and at smaller screen sizes added hamburger

--- src/Nav.js
+++ src/Nav.js
@@ -4,15 +4,20 @@ function Nav() {
       <li>
         <img alt="rebrandly logo" src="/img/rebrandly-logo.png" />
       </li>
-      <li className="ml-10">
+      <li className="ml-10 hidden lg:inline-block">
         <p>Features </p>
       </li>
-      <li className="ml-10">
+      <li className="ml-10 hidden lg:inline-block">
         <p>Pricing </p>
       </li>
-      <li className="ml-auto">
+      <li className="ml-auto hidden lg:inline-block">
         <p>Login </p>
       </li>
+      <div class="space-y-1 ml-auto lg:hidden ">
+        <div class="w-6 h-1 rounded bg-cyan-600"></div>
+        <div class="w-6 h-1 rounded bg-cyan-600"></div>
+        <div class="w-6 h-1 rounded bg-cyan-600"></div>
+      </div>
     </ul>
   );
 }
